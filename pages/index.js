cc22e6119fb3d71a835d1adf08ebd6edc6d221f2
Melvin Peepers <melvinpeepers@Melvins-MacBook-Pro.local>
2022-07-17 08:46:39 -0400

    added split text

--- pages/index.js
+++ pages/index.js
@@ -13,7 +13,7 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to Next.js! - Split
+          Welcome to Next.js! - this is the split
         </h1>
 
         <p className={styles.description}>
